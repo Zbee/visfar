93e6257eff8d51f4e4a82144cfbdc36e7a4f39eb
Ethan Henderson <ethan@zbee.me>
2015-03-31 23:18:16 -0600

    :clock2: Added debug mode and time estimation
    If the python is run with --debug argument then the average Wordnik (W)
    and Domainr (D) query times will be printed out.
    
    When the command is run it will estimate how long it will take to finish
    running based on the average time to do each word.

--- dicToCSV.py
+++ dicToCSV.py
@@ -1,11 +1,40 @@
 #simplejson is not standard (pip install simplejson)
-import urllib2, simplejson, sys, time, codecs
+import urllib2, simplejson, sys, time, codecs, argparse
 
-datetime = str(time.strftime("%Y-%m-%dT%H%M"))
+#setup debug
+parser = argparse.ArgumentParser(description='Convert a dictionary into a CSV with domain availability')
+parser.add_argument('--debug', help='if debug info should be shown', nargs='*')
+
+args = parser.parse_args()
+debug = False
+if args.debug is not None: debug = True
+
+#function to convert seconds to 
+def GetTime(sec):
+  m = 60
+  h = m*60
+  d = h*24
+  w = d*7
+  s = ""
+
+  weeks = sec//w
+  sec = sec - weeks*w
+  if weeks > 0: s += str(int(weeks)) + " weeks, "
+  days =  sec//d
+  sec = sec - days*d
+  if days > 0: s += str(int(days)) + " days, "
+  hours = sec//h
+  sec = sec - hours*h
+  if hours > 0: s += str(int(hours)) + " hours, "
+  minutes = sec//m
+  sec = sec - minutes*m
+  if minutes > 0: s += str(int(minutes)) + " minutes, "
+
+  return s[:-2]
 
 #function to create and manage a progress bar
 def update_progress(progress, status):
-  barLength = 50
+  barLength = 25
   status = status[0:20].ljust(20, " ")
   if isinstance(progress, int):
       progress = float(progress)
@@ -19,30 +48,43 @@ def update_progress(progress, status):
       progress = 1
       status = "Done...\r\n"
   block = int(round(barLength*progress))
-  text = "\rPercent: [{0}] {1}% {2}".format("#"*block + "-"*(barLength-block), progress*100, status)
+  tTC = GetTime((len(words) - count) * avgTime) + " to finish"
+  if debug:
+    avg = "W:" + str("%.3f" % avgWord) + ";D:" + str("%.3f" % avgDom)
+    text = "\rPercent: [{0}] {1}% ({2}) [{3}] Doing: {4}".format("#"*block + "-"*(barLength-block), "%.3f" % (progress*100), tTC, avg, status)
+  else:
+    text = "\rPercent: [{0}] {1}% ({2}) Doing: {3}".format("#"*block + "-"*(barLength-block), "%.3f" % (progress*100), tTC, status)
   sys.stdout.write(text)
   sys.stdout.flush()
+
+tOS = str(time.strftime("%Y-%m-%dT%H%M"))
+avgTime = 0.0
+avgWord = 0.0
+avgDom = 0.0
+count = 0.0
+goodWords = []
 
 #Load every word from SCOWL and friends
 words = open("en_US.dic") #Uses Hunspell dic http://wordlist.aspell.net/dicts/
 words = words.read().split("\n")
 
 #Make the output file have column headers
-text_file = open("visfarOutput" + datetime + ".csv", "w")
+text_file = open("visfarOutput" + tOS + ".csv", "w")
 text_file.write("word,definition,.com,.community,.company,.computer,.business,.center,.city,.enterprises,.host,.hosting,.limited,.net,.org,.solutions,.tech,.technology,.town,.co,.io,.compare,.corp,.inc,.ltd,\n")
 text_file.close()
 
 #Beginning
-print "Checking all words in SCOWL dictionary (" + str(len(words)) + " words) against Wordnik and Domainr to produce database of words, their definition, and if a few domains of that word are available"
+print "Checking all words in dictionary (" + str(len(words)) + " words) against Wordnik and Domainr"
+print "Started at " + tOS
 update_progress(0, "Starting")
-
-goodWords = []
-
-count = 0.0
 
 #Check words
 for word in words:
   count += 1.0
+
+  tOR = time.time()
+  wordTime = time.time()
+
   #See if the word is on Wordnik (because we need the description)
   req = urllib2.Request(
     "http://api.wordnik.com:80/v4/word.json/" + word + "/definitions?limit=1&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5",
@@ -54,6 +96,13 @@ for word in words:
   try:
     json = opener.open(req)
     data = simplejson.load(json)
+
+    wordTime = time.time() - wordTime
+    if avgWord == 0:
+      avgWord = wordTime
+    else:
+      avgWord = (avgWord + wordTime) / 2
+
     #If at least 1 definition was found on Wordnik
     if len(data) > 0:
       #Add the word to the list of good words
@@ -61,6 +110,9 @@ for word in words:
       #Clean up the word and its definition
       Sword = data[0]["word"].encode("utf-8")
       Stext = data[0]["text"].encode("utf-8")
+
+      domTime = time.time()
+
       #Now search Domainr for the word
       req = urllib2.Request(
         "https://domainr.com/api/json/search?client_id=visfar&q=" + word + ".com",
@@ -70,6 +122,13 @@ for word in words:
       #Read the data from Domainr
       json = opener.open(req)
       data = simplejson.load(json)
+
+      domTime = time.time() - domTime
+      if avgDom == 0:
+        avgDom = domTime
+      else:
+        avgDom = (avgDom + domTime) / 2
+
       results = data["results"]
       domAvail = {
         ".com": "",
@@ -111,9 +170,15 @@ for word in words:
       domAvailT = ""
       for (domain, availability) in domAvail.items():
         domAvailT += availability + ","
-      with open("visfarOutput" + datetime + ".csv", 'a') as f:
+      with open("visfarOutput" + tOS + ".csv", 'a') as f:
         #Add the word, its definition, and its domain availability to the output
         f.write(Sword + "," + Stext.replace(",", "`") + "," + domAvailT + "\n")
+
+        tOR = time.time() - tOR
+        if avgTime == 0:
+          avgTime = tOR
+        else:
+          avgTime = (avgTime + tOR) / 2
   #If the data from the website cannot be read (404, primarily)
   except urllib2.HTTPError as e:
     #Just skip it, no one cares in this case
